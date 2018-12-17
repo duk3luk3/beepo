e009de36205d20b0d1f43ce46bec2ecb2bf0078f
Lukas Erlacher <erlacher@in.tum.de>
2018-12-17 18:03:22 +1100

    Add feedback and timing

--- beepo/train.py
+++ beepo/train.py
@@ -1,7 +1,9 @@
 import sys
 import morse
+import time
 
 koch_span = 4
+cpm=15*4
 
 morse.play_init()
 
@@ -16,12 +18,34 @@ for letter in letters:
 
 letter = None
 
+correct_letters = 0
+all_letters = 0
+
 while True:
     letter = morse.koch_letter(koch_span, letter)
 
     morse.play_code(code_table, sounds, letter)
 
+    tm = time.time()
+
     letter_read = input()
 
-    print(letter)
+    ss = '!'
+
+    if letter.lower() == letter_read.lower():
+        dt = time.time() - tm
+        if dt <= (60 / cpm):
+            ss = '.'
+            correct_letters = correct_letters + 1
+        else:
+            ss = '... {0} / {1}'.format(dt, 60/cpm)
+    all_letters = all_letters + 1
+
+
+
+    print(letter + ss)
+
+    if all_letters % 10 == 0:
+        corr_perc = correct_letters / all_letters * 100
+        print("Performance: {0}%".format(corr_perc))
 
