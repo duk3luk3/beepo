13912fcb2ba7c73ec9a905a4983ea0d1b7de5b04
Lukas Erlacher <erlacher@in.tum.de>
2018-12-21 16:34:34 +1100

    Refactor performance measurement and feedback

--- beepo/train.py
+++ beepo/train.py
@@ -2,6 +2,8 @@ import sys
 import morse
 import time
 
+from enum import Enum
+
 koch_span = 4
 cpm = 15 * 4
 
@@ -21,6 +23,23 @@ letter = None
 correct_letters = 0
 all_letters = 0
 
+
+class CheckResult(Enum):
+    OK = 1
+    TOO_SLOW = 2
+    WRONG = 3
+
+
+def check_letter(letter, letter_read, dt):
+    if letter.lower() == letter_read.lower():
+        if dt <= 60 / cpm:
+            return CheckResult.OK
+        else:
+            return CheckResult.TOO_SLOW
+    else:
+        return CheckResult.WRONG
+
+
 while True:
     letter = morse.koch_letter(koch_span, letter)
 
@@ -30,18 +49,19 @@ while True:
 
     letter_read = input()
 
-    ss = '!'
+    dt = time.time() - tm
 
-    if letter.lower() == letter_read.lower():
-        dt = time.time() - tm
-        if dt <= (60 / cpm):
-            ss = '.'
-            correct_letters = correct_letters + 1
-        else:
-            ss = '... {0} / {1}'.format(dt, 60/cpm)
+    check_result = check_letter(letter, letter_read, dt)
+
     all_letters = all_letters + 1
 
-    print(letter + ss)
+    if check_result == CheckResult.OK:
+        print(letter + '.')
+        correct_letters = correct_letters + 1
+    elif check_result == CheckResult.TOO_SLOW:
+        print('{0}... {1} / {2}'.format(letter, dt, 60/cpm))
+    else:
+        print('{0}!'.format(letter))
 
     if all_letters % 10 == 0:
         corr_perc = correct_letters / all_letters * 100
